6a0748e4da7889881dcb25c4a0a0656e45540c07
yolalian <yolandaesterbrtg@gmail.com>
2023-08-04 23:06:39 +0700

    feat : new file array.js

---
+++ array.js
@@ -0,0 +1,30 @@
+const login = [
+    ["user1", "password123", "user1@example.com"],
+    ["user2", "secret456", "user2@example.com"]
+  ];
+  
+  console.log(login);
+
+  console.log()
+  
+  const stock = [
+      [1,"pen", "ada", 100, "tersedia", "gambar1", "pcs"],
+      [2, "pencil", "tidak ada", 0, "tidak tersedia", "gambar 2", "pcs"]
+  ];
+  
+  console.log (stock);
+  
+  const barangMasuk = [
+      [1, 1, 20-10-2003, "masuk1", "gambar1", "pcs"],
+      [2, 2, 11-04-2002, "masuk2", "gambar2", "pcs"]
+      
+  ];
+  console.log (barangMasuk);
+  
+  const barangKeluar = [
+      [1, 1, 20-10-2003, "konsumen1", "gambar1", "pcs"],
+      [2, 2, 11-04-2002, "konsumen2", "gambar2", "pcs"]
+  
+  ]
+  
+  console.log (barangKeluar);
