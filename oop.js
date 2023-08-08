3f47fcb78219249c5ae19770b3d3eac606987145
yolalian <yolandaesterbrtg@gmail.com>
2023-08-08 23:17:00 +0700

    feat : modify text

--- oop.js
+++ oop.js
@@ -1,32 +1,32 @@
-//table User
+//table ser
 class User {
   constructor(username, password) {
     this.username = username;
     this.password = password;
   }
-
+ 
   userLogin() {
     console.log(`${this.username} ${this.password} berhasil login`);
   }
-
+ 
   userLogout() {
     console.log(`${this.username} berhasil logout`);
   }
 }
 
-const user1 = new User("Yolanda", 20102003);
-const user2 = new User("Srimul", 11042002);
-
+const user1 = new User('Yolanda', 20102003);
+const user2 = new User('Srimul', 11042002);
+ 
 console.log(user1);
 console.log(user2);
-
+ 
 user1.userLogin();
 user1.userLogout();
 
 user2.userLogin();
 user2.userLogout();
 
-console.log();
+console.log()
 
 //table Stock Barang
 class StockBarang {
@@ -38,33 +38,23 @@ class StockBarang {
     this.status = status;
     this.satuan = satuan;
   }
-
+ 
   barangBertambah() {
-    console.log(
-      `${this.idbarang} ${this.namabarang} ${this.deskripsi} ${this.stock} ${this.status} ${this.satuan} barang berhasil bertambah`
-    );
+    console.log(`${this.idbarang} ${this.namabarang} ${this.deskripsi} ${this.stock} ${this.status} ${this.satuan} barang berhasil bertambah`);
   }
-
-  barangBerkurang() {
-    console.log(
-      `${this.idbarang} ${this.namabarang} ${this.deskripsi} ${this.stock} ${this.status} ${this.satuan} barang berhasil berkurang`
-    );
+ 
+   barangBerkurang() {
+    console.log(`${this.idbarang} ${this.namabarang} ${this.deskripsi} ${this.stock} ${this.status} ${this.satuan} barang berhasil berkurang`);
   }
+  
 }
 
-const stockbarang1 = new StockBarang("1", "Pen", "Ada", 100, "tersedia", "pcs");
-const stockbarang2 = new StockBarang(
-  "2",
-  "Pencil",
-  "Tidak ada",
-  0,
-  "tidak tersedia",
-  "pcs"
-);
-
+const stockbarang1 = new StockBarang('1', 'Pen', 'Ada', 100,'tersedia', 'pcs');
+const stockbarang2 = new StockBarang('2', 'Pencil', 'Tidak ada', 0,'tidak tersedia', 'pcs');
+ 
 console.log(stockbarang1);
 console.log(stockbarang2);
-
+ 
 stockbarang1.barangBertambah();
 stockbarang2.barangBerkurang();
 
@@ -97,5 +87,7 @@ const barangmasuk = new BarangMasuk(
 console.log(barangmasuk);
 
 barangmasuk.daftarBarang();
+
+console.log();
 
 console.log();
