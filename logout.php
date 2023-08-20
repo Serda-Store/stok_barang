d765b5a053ec37a53a934c7e003b91036d7f1acf
yolalian <yolandaesterbrtg@gmail.com>
2023-08-20 22:28:35 +0700

    feat : modify text

--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $_SESSION['session_username'] = "";
@@ -16,22 +15,3 @@ $time = time() - (60 * 60);
 setcookie($cookie_name, $cookie_value, $time, "/");
 
 header("location:login.php");
-=======
-<?php
-session_start();
-$_SESSION['session_username'] = "";
-$_SESSION['session_password'] = "";
-session_destroy();
-
-$cookie_name = "cookie_username";
-$cookie_value = "";
-$time = time() - (60 * 60);
-setcookie($cookie_name, $cookie_value, $time, "/");
-
-$cookie_name = "cookie_password";
-$cookie_value = "";
-$time = time() - (60 * 60);
-setcookie($cookie_name, $cookie_value, $time, "/");
-
-header("location:login.php");
->>>>>>> e7d21f8a37fb862f4263612d557d8b194a48e349
