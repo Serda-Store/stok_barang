8a825082be3e38e8150dd0c3c8d5a2441bba26ff
yolalian <yolandaesterbrtg@gmail.com>
2023-08-24 22:10:16 +0700

    feat : create new file ini folder config.

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+$host_db    = "localhost";
+$user_db    = "root";
+$pass_db    = "";
+$nama_db    = "serda_store";
+$koneksi    = mysqli_connect($host_db, $user_db, $pass_db, $nama_db);
+
+if (!$koneksi) {
+    echo 'Koneksi gagal terhubung. Pesan error :' .  mysqli_connect_errno();
+}
