3a0a3e5be4d397590ee1a197a03a2b3c0c27940d
yolalian <yolandaesterbrtg@gmail.com>
2023-08-24 22:24:46 +0700

    feat : modify text.

--- stok_barang.php
+++ stok_barang.php
@@ -1,58 +1,65 @@
 <?php
 session_start();
-print_r($_SESSION);
-print_r($_COOKIE);
+ob_start();
 
+include '../config/db.php';
+
+if (empty($_SESSION['username']) or empty($_SESSION['password'])) {
+    echo "Anda harus login terlebih dahulu!";
+    echo "<a href='../login.php'>Login</a>";
+} else {
+    define('INDEX', true);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Admin</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Halaman Admin</title>
+    </head>
 
-<body>
-    <h1>Halo, <?= $_SESSION['username']; ?></h1>
-    <div>
-        <h2>STOK BARANG SERDA</h2>
-        <table border="1">
-            <thead>
-                <th>No</th>
-                <th>Nama Barang</th>
-                <th>Deskripsi</th>
-                <th>Jumlah</th>
-            </thead>
-            <tbody>
-                <?php
-                $query = mysqli_query($koneksi, "SELECT * FROM stock");
-                $no = 0;
-                while ($data = mysqli_fetch_array($query)) {
-                    $no++;
-                ?>
-                    <tr>
-                        <td><?= $no; ?></td>
-                        <td><?= $data['nama_barang']; ?></td>
-                        <td><?= $data['deskripsi']; ?></td>
-                        <td><?= $data['stock']; ?></td>
-                        <td><a href="#">Edit</a> | <a href="#">Hapus</a></td>
-                    </tr>
+    <body>
+        <h1>Halo, <?= $_SESSION['username']; ?></h1>
+        <div>
+            <h2>STOK BARANG SERDA</h2>
+            <table border="1">
+                <thead>
+                    <th>No</th>
+                    <th>Nama Barang</th>
+                    <th>Deskripsi</th>
+                    <th>Jumlah</th>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = mysqli_query($koneksi, "SELECT * FROM stock");
+                    $no = 0;
+                    while ($data = mysqli_fetch_array($query)) {
+                        $no++;
+                    ?>
+                        <tr>
+                            <td><?= $no; ?></td>
+                            <td><?= $data['nama_barang']; ?></td>
+                            <td><?= $data['deskripsi']; ?></td>
+                            <td><?= $data['stock']; ?></td>
+                            <td><a href="#">Edit</a> | <a href="#">Hapus</a></td>
+                        </tr>
 
-                <?php
-                }
-                ?>
-            </tbody>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+
         </table>
-    </div>
+        </div>
+        <a href="../logout.php">Logout</a>
+    </body>
 
-    </table>
-    </div>
-    <a href="../logout.php">Logout</a>
-</body>
-
-</html>
+    </html>
 
 <?php
+}
 ?>
